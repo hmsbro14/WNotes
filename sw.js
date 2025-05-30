1d7e6f01025da168b968a55d5e95f6c8d98ebfd8
hmsbro14 <harrisonmikesmith14@gmail.com>
2025-05-30 12:07:44 -0500

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,20 @@
+const cacheName = 'app-v1';
+const filesToCache = [
+  'index.html',
+  'manifest.json',
+  'sw.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(cacheName)
+      .then(cache => cache.addAll(filesToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
